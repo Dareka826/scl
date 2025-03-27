15db65570d6b24cb2acd41b6681156e2ba68ed68
Rin Brk <dareka826@gmail.com>
2025-03-27 18:34:45 +0100

    Add header with default flags

---
+++ src/flags.h
@@ -0,0 +1,10 @@
+#ifndef __SCL_FLAGS_H_jpPPqyG4
+#define __SCL_FLAGS_H_jpPPqyG4
+
+#if defined(SCL_NO_NULL_ERR_WARNINGS)
+#  undef  SCL_NULL_ERR_WARN
+#else
+#  define SCL_NULL_ERR_WARN
+#endif
+
+#endif
