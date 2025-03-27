d83e0ea4a784d4ccd2e7cd11f167089b678335d1
Rin Brk <dareka826@gmail.com>
2025-03-27 18:33:37 +0100

    Add a types header

---
+++ src/types.h
@@ -0,0 +1,12 @@
+#ifndef __SCL_TYPES_H_rMjLTa8W
+#define __SCL_TYPES_H_rMjLTa8W
+
+#include "gen_types.h"
+#include <stddef.h>
+
+typedef unsigned char umin_t;
+typedef   signed char imin_t;
+
+typedef umin_t bool_t;
+
+#endif
