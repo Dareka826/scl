f5be46d3b2c996b8d48fe463c37f72fe7daae91b
Rin Brk <dareka826@gmail.com>
2025-03-27 18:41:08 +0100

    Add errors header and alloc templates

---
+++ src/errors.h
@@ -0,0 +1,11 @@
+#ifndef __SCL_ERRORS_H_rGcAaVlR
+#define __SCL_ERRORS_H_rGcAaVlR
+
+enum {
+    SCL_ERR_OK = 0,
+
+    SCL_ERR_INVALID_ID           = 1 << 0,
+    SCL_ERR_INVALID_ALLOC_STRUCT = 1 << 1,
+};
+
+#endif
