26bdfe1187aed5047e75c9b8efc24feea93300f4
Rin Brk <dareka826@gmail.com>
2025-03-29 14:14:57 +0100

    Update detect.c

--- src/detect.c
+++ src/detect.c
@@ -2,11 +2,28 @@
 #include <stdlib.h> /* exit() EXIT_FAILURE */
 #include <stddef.h> /* NULL */
 #include <string.h> /* strlen() */
+#include <limits.h> /* CHAR_BIT */
+#include "assert.h" /* _assert() */
 
-#define _assert(expr) if (!(expr)) exit(EXIT_FAILURE);
-#define MAX_LINE_LEN 255
 
-typedef unsigned int uint_t;
+/* Count the number of bits that can fit in a given type without relying on
+ * bit shifting or -1 (not sure if they behave the same on all platforms) */
+#define numbits(int_type, out_var_ptr) \
+    do { \
+        unsigned int_type x = 0, old_x; \
+        *(out_var_ptr) = 0; \
+        \
+        while (1) { \
+            old_x = x; \
+            x = (x * 2) + 1; \
+            if (x <= old_x) break; \
+            *(out_var_ptr) += 1; \
+        } \
+    } while (0)
+
+#define MAX_LINE_LEN 254
+
+typedef unsigned long ulong;
 
 enum {
     STATUS_OK = 0,
@@ -23,27 +40,72 @@ void _fwrite(FILE* file, char *buf, size_t count, int *err) {
         *err |= ERR_WRITE;
 }
 
-#define WRITE_CHR(file, chr, p_err) do { if (fputc((chr), (file)) != (chr)) *(p_err) |= ERR_WRITE; } while(0)
-#define WRITE_STR(file, str, p_err) do { _fwrite((file), (str), strlen((str)), (p_err)); } while(0)
+#define WRITE_CHR(file, chr, p_err) \
+    do { \
+        if (fputc((chr), (file)) != (chr)) \
+            *(p_err) |= ERR_WRITE; \
+    } while(0)
+
+#define WRITE_STR(file, str, p_err) \
+    _fwrite((file), (str), strlen((str)), (p_err))
+
+enum {
+    SIZE_ID_CHAR        = 0,
+    SIZE_ID_SHORT       = 1,
+    SIZE_ID_INT         = 2,
+    SIZE_ID_LONG        = 3,
+    SIZE_ID_FLOAT       = 4,
+    SIZE_ID_DOUBLE      = 5,
+    SIZE_ID_LONG_DOUBLE = 6,
+    SIZE_ID_POINTER     = 7,
+
+    SIZE_ID_ELEM_COUNT
+};
+
 
 int main(void) {
-    uint_t char_bytes = 0, short_bytes = 0, int_bytes = 0, long_bytes = 0, 
-           float_bytes = 0, double_bytes = 0, long_double_bytes = 0,
-           ptr_bytes = 0;
     char buf[MAX_LINE_LEN + 1];
     int char_count = 0, err = 0;
 
-    /* Detect sizes */
-    char_bytes        = sizeof(char);
-    short_bytes       = sizeof(short);
-    int_bytes         = sizeof(int);
-    long_bytes        = sizeof(long);
-    float_bytes       = sizeof(float);
-    double_bytes      = sizeof(double);
-    long_double_bytes = sizeof(long double);
-    ptr_bytes         = sizeof(void*);
+    ulong  bits[SIZE_ID_ELEM_COUNT];
+    ulong bytes[SIZE_ID_ELEM_COUNT];
+    ulong bpb = 0; /* bits per byte */
 
-    /* Write output */
+    /* Check for sane char
+     * Any C89 conformant system should meet these */
+    numbits(char,  &(bits[SIZE_ID_CHAR]));
+    _assert(bits[SIZE_ID_CHAR] >= 8);
+
+    bytes[SIZE_ID_CHAR] = sizeof(char);
+    _assert(bytes[SIZE_ID_CHAR] == 1);
+
+    /* The char type should represent host's byte
+     * CHAR_BIT is bits per byte */
+    bpb = bits[SIZE_ID_CHAR];
+    _assert(CHAR_BIT == bpb);
+
+    /* Detect other sizes */
+    numbits(short, &(bits[SIZE_ID_SHORT]));
+    bytes[SIZE_ID_SHORT] = sizeof(short);
+
+    numbits(int,   &(bits[SIZE_ID_INT]));
+    bytes[SIZE_ID_INT] = sizeof(int);
+
+    numbits(long,  &(bits[SIZE_ID_LONG]));
+    bytes[SIZE_ID_LONG] = sizeof(long);
+
+    bytes[SIZE_ID_FLOAT] = sizeof(float);
+    bits[SIZE_ID_FLOAT]  = bytes[SIZE_ID_FLOAT] * bits[SIZE_ID_CHAR];
+
+    bytes[SIZE_ID_DOUBLE] = sizeof(double);
+    bits[SIZE_ID_DOUBLE]  = bytes[SIZE_ID_DOUBLE] * bits[SIZE_ID_CHAR];
+
+    bytes[SIZE_ID_LONG_DOUBLE] = sizeof(long double);
+    bits[SIZE_ID_LONG_DOUBLE]  = bytes[SIZE_ID_LONG_DOUBLE] * bits[SIZE_ID_CHAR];
+
+    bytes[SIZE_ID_POINTER] = sizeof(void*);
+    bits[SIZE_ID_POINTER]  = bytes[SIZE_ID_POINTER] * bits[SIZE_ID_CHAR];
+
     {
         FILE *f_sizes = NULL;
         f_sizes = fopen("./gen_sizes.h", "w");
@@ -56,28 +118,40 @@ int main(void) {
 
         WRITE_CHR(f_sizes, '\n', &err);
 
-        char_count = sprintf(buf, "#define CHAR_BYTES       %u\n", char_bytes);
+        char_count = sprintf(buf, "#define CHAR_BYTES       %lu\n", bytes[SIZE_ID_CHAR]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define SHORT_BYTES      %lu\n", bytes[SIZE_ID_SHORT]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define INT_BYTES        %lu\n", bytes[SIZE_ID_INT]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define LONG_BYTES       %lu\n", bytes[SIZE_ID_LONG]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define FLOAT_BYTES      %lu\n", bytes[SIZE_ID_FLOAT]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define DOUBLE_BYTES     %lu\n", bytes[SIZE_ID_DOUBLE]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define LDOUBLE_BYTES    %lu\n", bytes[SIZE_ID_LONG_DOUBLE]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define PTR_BYTES        %lu\n", bytes[SIZE_ID_POINTER]);
         _fwrite(f_sizes, buf, char_count, &err);
 
-        char_count = sprintf(buf, "#define SHORT_BYTES      %u\n", short_bytes);
-        _fwrite(f_sizes, buf, char_count, &err);
+        WRITE_CHR(f_sizes, '\n', &err);
 
-        char_count = sprintf(buf, "#define INT_BYTES        %u\n", int_bytes);
+        char_count = sprintf(buf, "#define CHAR_BITS        %lu\n", bits[SIZE_ID_POINTER]);
         _fwrite(f_sizes, buf, char_count, &err);
-
-        char_count = sprintf(buf, "#define LONG_BYTES       %u\n", long_bytes);
+        char_count = sprintf(buf, "#define SHORT_BITS       %lu\n", bits[SIZE_ID_SHORT]);
         _fwrite(f_sizes, buf, char_count, &err);
-
-        char_count = sprintf(buf, "#define FLOAT_BYTES      %u\n", float_bytes);
+        char_count = sprintf(buf, "#define INT_BITS         %lu\n", bits[SIZE_ID_INT]);
         _fwrite(f_sizes, buf, char_count, &err);
-
-        char_count = sprintf(buf, "#define DOUBLE_BYTES     %u\n", double_bytes);
+        char_count = sprintf(buf, "#define LONG_BITS        %lu\n", bits[SIZE_ID_LONG]);
         _fwrite(f_sizes, buf, char_count, &err);
-
-        char_count = sprintf(buf, "#define LDOUBLE_BYTES    %u\n", long_double_bytes);
+        char_count = sprintf(buf, "#define FLOAT_BITS       %lu\n", bits[SIZE_ID_FLOAT]);
         _fwrite(f_sizes, buf, char_count, &err);
-
-        char_count = sprintf(buf, "#define PTR_BYTES        %u\n", ptr_bytes);
+        char_count = sprintf(buf, "#define DOUBLE_BITS      %lu\n", bits[SIZE_ID_DOUBLE]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define LDOUBLE_BITS     %lu\n", bits[SIZE_ID_LONG_DOUBLE]);
+        _fwrite(f_sizes, buf, char_count, &err);
+        char_count = sprintf(buf, "#define PTR_BITS         %lu\n", bits[SIZE_ID_POINTER]);
         _fwrite(f_sizes, buf, char_count, &err);
 
         WRITE_CHR(f_sizes, '\n', &err);
@@ -91,7 +165,6 @@ int main(void) {
             exit(err);
     }
 
-    /* Write output */
     {
         FILE *f_types = NULL;
         f_types = fopen("./gen_types.h", "w");
@@ -109,134 +182,83 @@ int main(void) {
          * int   is at least 16 bits
          * long  is at least 32 bits */
 
-        /* u8_t i8_t */
-        if (char_bytes == 1) {
-            WRITE_STR(f_types, "typedef unsigned char  u8_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed char  i8_t;\n", &err);
+        /*
+         * u8p  -> unsigned 8+  -> at least  8 bits (can be more)
+         * u16p -> unsigned 16+ -> at least 16 bits (can be more)
+         */
+
+        WRITE_STR(f_types, "typedef unsigned char  ubyte;\n", &err);
+        WRITE_STR(f_types, "typedef   signed char  ibyte;\n", &err);
+        WRITE_STR(f_types, "typedef unsigned char  u8p;\n", &err);
+        WRITE_STR(f_types, "typedef   signed char  i8p;\n", &err);
+
+        if (bits[SIZE_ID_CHAR] >= 16) {
+            WRITE_STR(f_types, "typedef unsigned char  u16p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed char  i16p;\n", &err);
+        } else if (bits[SIZE_ID_SHORT] >= 16) {
+            WRITE_STR(f_types, "typedef unsigned short u16p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed short i16p;\n", &err);
         }
 
-        /* u16_t i16_t */
-        if (char_bytes == 2) {
-            WRITE_STR(f_types, "typedef unsigned char  u16_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed char  i16_t;\n", &err);
-        } else if (short_bytes == 2) {
-            WRITE_STR(f_types, "typedef unsigned short u16_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed short i16_t;\n", &err);
-        } else if (int_bytes == 2) {
-            WRITE_STR(f_types, "typedef unsigned int   u16_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed int   i16_t;\n", &err);
+        if (bits[SIZE_ID_CHAR] >= 32) {
+            WRITE_STR(f_types, "typedef unsigned char  u32p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed char  i32p;\n", &err);
+        } else if (bits[SIZE_ID_SHORT] >= 32) {
+            WRITE_STR(f_types, "typedef unsigned short u32p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed short i32p;\n", &err);
+        } else if (bits[SIZE_ID_INT] >= 32) {
+            WRITE_STR(f_types, "typedef unsigned int   u32p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed int   i32p;\n", &err);
+        } else if (bits[SIZE_ID_LONG] >= 32) {
+            WRITE_STR(f_types, "typedef unsigned long  u32p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed long  i32p;\n", &err);
         }
 
-        /* u32_t i32_t */
-        if (char_bytes == 4) {
-            WRITE_STR(f_types, "typedef unsigned char  u32_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed char  i32_t;\n", &err);
-        } else if (short_bytes == 4) {
-            WRITE_STR(f_types, "typedef unsigned short u32_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed short i32_t;\n", &err);
-        } else if (int_bytes == 4) {
-            WRITE_STR(f_types, "typedef unsigned int   u32_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed int   i32_t;\n", &err);
-        } else if (long_bytes == 4) {
-            WRITE_STR(f_types, "typedef unsigned long  u32_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed long  i32_t;\n", &err);
+        if (bits[SIZE_ID_CHAR] >= 64) {
+            WRITE_STR(f_types, "typedef unsigned char  u64p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed char  i64p;\n", &err);
+        } else if (bits[SIZE_ID_SHORT] >= 64) {
+            WRITE_STR(f_types, "typedef unsigned short u64p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed short i64p;\n", &err);
+        } else if (bits[SIZE_ID_INT] >= 64) {
+            WRITE_STR(f_types, "typedef unsigned int   u64p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed int   i64p;\n", &err);
+        } else if (bits[SIZE_ID_LONG] >= 64) {
+            WRITE_STR(f_types, "typedef unsigned long  u64p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed long  i64p;\n", &err);
         }
 
-        /* u64_t i64_t */
-        if (char_bytes == 8) {
-            WRITE_STR(f_types, "typedef unsigned char  u64_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed char  i64_t;\n", &err);
-        } else if (short_bytes == 8) {
-            WRITE_STR(f_types, "typedef unsigned short u64_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed short i64_t;\n", &err);
-        } else if (int_bytes == 8) {
-            WRITE_STR(f_types, "typedef unsigned int   u64_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed int   i64_t;\n", &err);
-        } else if (long_bytes == 8) {
-            WRITE_STR(f_types, "typedef unsigned long  u64_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed long  i64_t;\n", &err);
-        }
-
-        /* u128_t i128_t */
-        if (char_bytes == 16) {
-            WRITE_STR(f_types, "typedef unsigned char  u128_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed char  i128_t;\n", &err);
-        } else if (short_bytes == 16) {
-            WRITE_STR(f_types, "typedef unsigned short u128_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed short i128_t;\n", &err);
-        } else if (int_bytes == 16) {
-            WRITE_STR(f_types, "typedef unsigned int   u128_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed int   i128_t;\n", &err);
-        } else if (long_bytes == 16) {
-            WRITE_STR(f_types, "typedef unsigned long  u128_t;\n", &err);
-            WRITE_STR(f_types, "typedef   signed long  i128_t;\n", &err);
+        if (bits[SIZE_ID_CHAR] >= 128) {
+            WRITE_STR(f_types, "typedef unsigned char  u128p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed char  i128p;\n", &err);
+        } else if (bits[SIZE_ID_SHORT] >= 128) {
+            WRITE_STR(f_types, "typedef unsigned short u128p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed short i128p;\n", &err);
+        } else if (bits[SIZE_ID_INT] >= 128) {
+            WRITE_STR(f_types, "typedef unsigned int   u128p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed int   i128p;\n", &err);
+        } else if (bits[SIZE_ID_LONG] >= 128) {
+            WRITE_STR(f_types, "typedef unsigned long  u128p;\n", &err);
+            WRITE_STR(f_types, "typedef   signed long  i128p;\n", &err);
         }
 
         WRITE_CHR(f_types, '\n', &err);
 
-        /* f8_t */
-        if (float_bytes == 1) {
-            WRITE_STR(f_types, "typedef float  f8_t;\n", &err);
-        } else if (double_bytes == 1) {
-            WRITE_STR(f_types, "typedef double f8_t;\n", &err);
-        } else if (long_double_bytes == 1) {
-            WRITE_STR(f_types, "typedef long double f8_t;\n", &err);
-        }
-
-        /* f16_t */
-        if (float_bytes == 2) {
-            WRITE_STR(f_types, "typedef float  f16_t;\n", &err);
-        } else if (double_bytes == 2) {
-            WRITE_STR(f_types, "typedef double f16_t;\n", &err);
-        } else if (long_double_bytes == 2) {
-            WRITE_STR(f_types, "typedef long double f16_t;\n", &err);
-        }
-
-        /* f32_t */
-        if (float_bytes == 4) {
-            WRITE_STR(f_types, "typedef float  f32_t;\n", &err);
-        } else if (double_bytes == 4) {
-            WRITE_STR(f_types, "typedef double f32_t;\n", &err);
-        } else if (long_double_bytes == 4) {
-            WRITE_STR(f_types, "typedef long double f32_t;\n", &err);
-        }
-
-        /* f64_t */
-        if (float_bytes == 8) {
-            WRITE_STR(f_types, "typedef float  f64_t;\n", &err);
-        } else if (double_bytes == 8) {
-            WRITE_STR(f_types, "typedef double f64_t;\n", &err);
-        } else if (long_double_bytes == 8) {
-            WRITE_STR(f_types, "typedef long double f64_t;\n", &err);
-        }
-
-        /* f128_t */
-        if (float_bytes == 16) {
-            WRITE_STR(f_types, "typedef float  f128_t;\n", &err);
-        } else if (double_bytes == 16) {
-            WRITE_STR(f_types, "typedef double f128_t;\n", &err);
-        } else if (long_double_bytes == 16) {
-            WRITE_STR(f_types, "typedef long double f128_t;\n", &err);
-        }
-
-        WRITE_CHR(f_types, '\n', &err);
-
-        /* usize isize */
-        if (ptr_bytes == 1) {
-            WRITE_STR(f_types, "typedef u8_t usize_t;\n", &err);
-            WRITE_STR(f_types, "typedef i8_t isize_t;\n", &err);
-        } else if (ptr_bytes == 2) {
-            WRITE_STR(f_types, "typedef u16_t usize_t;\n", &err);
-            WRITE_STR(f_types, "typedef i16_t isize_t;\n", &err);
-        } else if (ptr_bytes == 4) {
-            WRITE_STR(f_types, "typedef u32_t usize_t;\n", &err);
-            WRITE_STR(f_types, "typedef i32_t isize_t;\n", &err);
-        } else if (ptr_bytes == 8) {
-            WRITE_STR(f_types, "typedef u64_t usize_t;\n", &err);
-            WRITE_STR(f_types, "typedef i64_t isize_t;\n", &err);
-        } else if (ptr_bytes == 16) {
-            WRITE_STR(f_types, "typedef u128_t usize_t;\n", &err);
-            WRITE_STR(f_types, "typedef i128_t isize_t;\n", &err);
+        if (bytes[SIZE_ID_POINTER] == bytes[SIZE_ID_LONG]) {
+            WRITE_STR(f_types, "typedef unsigned long usize;\n", &err);
+            WRITE_STR(f_types, "typedef   signed long isize;\n", &err);
+        } else if (bytes[SIZE_ID_POINTER] == bytes[SIZE_ID_INT]) {
+            WRITE_STR(f_types, "typedef unsigned int usize;\n", &err);
+            WRITE_STR(f_types, "typedef   signed int isize;\n", &err);
+        } else if (bytes[SIZE_ID_POINTER] == bytes[SIZE_ID_SHORT]) {
+            WRITE_STR(f_types, "typedef unsigned short usize;\n", &err);
+            WRITE_STR(f_types, "typedef   signed short isize;\n", &err);
+        } else if (bytes[SIZE_ID_POINTER] == bytes[SIZE_ID_CHAR]) {
+            WRITE_STR(f_types, "typedef unsigned char usize;\n", &err);
+            WRITE_STR(f_types, "typedef   signed char isize;\n", &err);
+        } else {
+            fputs("[E]: Pointer type size doesn't match any int type!\n", stderr);
+            return EXIT_FAILURE;
         }
 
         WRITE_CHR(f_types, '\n', &err);
